078ec5f6728915a636b7627f4e3ac696b86705b2
Illya Gordiyenko <illyagordy@gmail.com>
2018-04-25 21:23:27 -0400

    Initial Commit

---
+++ WordParser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordParser
+//
+//  Created by Illya Gordiyenko on 2018-04-25.
+//  Copyright © 2018 Illya Gordiyenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
